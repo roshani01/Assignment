7fdafae1fcd0a7c71f152d5765be0f14f07f1acc
roshani01 <roshanim075@gmail.com>
2024-11-17 16:08:31 +0530

    backend commit

--- assignment/assignment-portal/Backend/server/app.js
+++ assignment/assignment-portal/Backend/server/app.js
@@ -31,13 +31,25 @@ const http = require('http').Server(app);
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 
-mongoose.connect("mongodb+srv://<db_username>:8BQOhNNwVBAFbs2K@testpro.q8c0o.mongodb.net/?retryWrites=true&w=majority&appName=testpro")
+mongoose.connect("mongodb+srv://roshanim075:Roshani01@testpro.q8c0o.mongodb.net/?retryWrites=true&w=majority&appName=testpro")
  
 //const Assignment = require('../models/Assignment');
-const User = require('../models/User');  // Ensure the correct path
-//const connectDB = require('./config/database');
-//const authRoutes = require('./routes/authRoutes');
-//const assignmentRoutes = require('./routes/assignmentRoutes');
+const User = require('../models/User');
+async function insert() {
+    await User.create({
+        name:'roshani',
+        email:'roshanim075@gmail.com',
+        password:'roshani123',
+        role:'User'
+    });
+    
+}
+insert();
+
+// Ensure the correct path
+const connectDB = require('../config/database');
+const authRoutes = require('../routes/authRoutes');
+const assignmentRoutes = require('../routes/assignmentRoutes');
 
 
 http.listen(27017,function(){
